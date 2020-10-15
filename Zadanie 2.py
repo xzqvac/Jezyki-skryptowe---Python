3cf586d9cbed762c66e23b60c53406574c2a4bdd
xzqvac <kawkaradoslaw@gmail.com>
2020-10-15 09:23:32 +0200

    Zadanie 3

--- Zadanie 2.py
+++ Zadanie 2.py
@@ -1,2 +1,6 @@
-import cmath
-print "sin(3) : ",  math.sin(3)
+import math
+a=3
+b=4
+k=47
+P=(a*b*math.sin(k))/2
+print(P)
---
+++ Zadanie 3.py
@@ -0,0 +1,6 @@
+import math
+a = int(input())
+b = int(input())
+k = int(input())
+P=(a*b*math.sin(k))/2
+print(P)
