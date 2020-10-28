86dfa7644ac6addae7004b85da435457e8dff876
xzqvac <kawkaradoslaw@gmail.com>
2020-10-28 19:50:04 +0100

    Lista 1

---
+++ Lista 1/Zadanie 8.py
@@ -0,0 +1,6 @@
+a=float(input("Podaj liczbę a(wieksza) : "))
+b=float(input("Podaj liczbę b(mniejsza): "))
+Z=b%a
+print("Reszta z dzielenia liczby", b, "przez", a, "wynosi", Z)
+Z*=Z+3
+print("Wynik: ", Z)
