6cba525c747795e0bbb877a9eaf8ab64434e25fa
xzqvac <kawkaradoslaw@gmail.com>
2020-10-28 19:23:20 +0100

    Merge branch 'main' of D:\Studia UWr\Języki skryptowe - python\Lista1 with conflicts.

---
+++ Zadanie 5.py
@@ -0,0 +1,10 @@
+import math
+print("5//2 =", 5//2)
+print("Jest to dzielenie całkowite. Część dziesiętna jest pomijana")
+print("5/2 =", 5/2)
+print("Jest to dzielenie z czescia dziesietna ", "\n")
+print("round(5.497, 2) =", round(5.497, 2))
+print("round(5.497) =", round(5.497), "\n")
+
+print("(math.floor(5.999) =", math.floor(5.999))
+print("Największa Liczba całkowita nie większa niż podany argument")
---
+++ Zadanie1.py
@@ -0,0 +1,6 @@
+a = input()
+b = input()
+suma = a+b
+print(suma)
+print("Spodziewałem się innego wyniku.Wczytane liczby zostały wczytane jako typ łańcuch. " 
+      "Aby otrzymać wartość równania matematycznego a nie napisu należy dokonać konwersji na typ int - całkowity")
