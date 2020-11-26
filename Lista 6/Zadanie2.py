3b732be99fa95a456a0da09565992fe0294b7f4a
xzqvac <kawkaradoslaw@gmail.com>
2020-11-26 09:20:04 +0100

    Listy

--- Lista 6/Zadanie2.py
+++ Lista 6/Zadanie2.py
@@ -3,6 +3,8 @@ KLUCZ = 3
 
 def szyfruj(txt):
     zaszyfrowny = ""
+    txt = txt.lower()
+    print(txt)
     for i in range(len(txt)):
         if ord(txt[i]) > 122 - KLUCZ:
             zaszyfrowny += chr(ord(txt[i]) + KLUCZ - 26)
