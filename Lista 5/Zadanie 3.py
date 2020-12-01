f9fee54243f9c57ecb7f4a17b0ad1dcae4108699
xzqvac <kawkaradoslaw@gmail.com>
2020-12-01 14:44:23 +0100

    Update zadanie 3.py

--- Lista 5/Zadanie 3.py
+++ Lista 5/Zadanie 3.py
@@ -1,35 +1,31 @@
 def rom2dec(value):
     dec = {
         "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    result = 0
-    dec_list = list(dec.keys())
-    count_i = value.count("I")
-    count_x = value.count("X")
-    count_c = value.count("C")
-    count_m = value.count("M")
     length = len(value)
-    if count_c > 3 or count_i > 3 or count_m > 3 or count_x > 3:
-        print("Podałeś nieprawidłowy zapis. Zwróć uwagę na maksymalną ilość znaków I, X, C, M")
-    for i in range(0, length-1, 1):
-        if value[i] == "V" and (value[i + 1] == "D" or value[i + 1] == "D"):
-            print("Podales nieprawidlowy zapis. Zwróć uwagę, że litery V, L, D nie mogą stać obok siebie")
-            break
-        elif value[i] == "L" and (value[i + 1] == "V" or value[i + 1] == "D"):
-            print("Podales nieprawidlowy zapis. Zwróć uwagę, że litery V, L, D nie mogą stać obok siebie")
-            break
-        elif value[i] == "D" and (value[i + 1] == "L" or value[i + 1] == "V"):
-            print("Podales nieprawidlowy zapis. Zwróć uwagę, że litery V, L, D nie mogą stać obok siebie")
-            break
-    for i in range(0, length-1, 1):
-        if value[i] == "I" and (value[i + 1] == "I" or value[i + 1] == "L" or value[i + 1] == "C" or value[i + 1] == "D" or value[i + 1] == "M"):
-            print("Podałeś nieprawidłowy zapis. Litera I może tylko raz wystąpić gdy liczba jest zmniejszana")
-            break
-        if value[i] == "X" and (value[i + 1] == "X" or value[i + 1] == "V" or value[i + 1] == "X" or value[i + 1] == "D" or value[i + 1] == "M"):
-            print("Podałeś nieprawidłowy zapis. Litera X może tylko raz wystąpić gdy liczba jest zmniejszana")
-            break
-        if value[i] == "C" and (value[i + 1] == "C" or value[i + 1] == "V" or value[i + 1] == "X" or value[i + 1] == "L" or value[i + 1] == "C"):
-            print("Podałeś nieprawidłowy zapis. Litera C może tylko raz wystąpić gdy liczba jest zmniejszana")
-            break
+    if value.count("I") > 3 or value.count("X") > 3 or value.count("C") > 3 or value.count("M") > 3:
+        return "Podałeś nieprawidłowy zapis. Zwróć uwagę na maksymalną ilość znaków I, X, C, M"
+    for i in range(0, length - 1, 1):
+        if value[i] == "V" and value[i + 1] == "V":
+            return "Podales nieprawidlowy zapis. Zwróć uwagę, że litera V nie może stać obok L i D i siebie samej"
+        elif value[i] == "L" and value[i + 1] == "L":
+            return "Podales nieprawidlowy zapis. Zwróć uwagę, że litera L nie może stać obok V i D i siebie samej"
+        elif value[i] == "D" and value[i + 1] == "D":
+            return "Podales nieprawidlowy zapis. Zwróć uwagę, że litera D nie może stać obok L i V i siebie samej"
+    if length >= 3:
+        for i in range(length, 2, -1):
+            if dec.get(value[i - 1]) > dec.get(value[i - 2]) == dec.get(value[i - 3]):
+                return "Podałeś nieprawidłowy zapis. Nie może wystąpić tyle znaków poprzedzających"
+    for i in range(0, length - 1, 1):
+        if dec.get(value[i + 1]) > dec.get(value[i]):
+            if value[i] == "I" and (value[i + 1] == "L" or value[i + 1] == "C" or value[i + 1] == "D" or value[i + 1] == "M"):
+                return "Podales nieprawidlowy zapis. Zle zmniejszasz I"
+            elif value[i] == "X" and (value[i + 1] == "V" or value[i + 1] == "X" or value[i + 1] == "D" or value[i + 1] == "M"):
+                return "Podales nieprawidlowy zapis. Zle zmniejszasz X"
+            elif value[i] == "C" and (value[i + 1] != "V" or value[i + 1] != "X" or value[i + 1] != "L" or value[i + 1] != "C"):
+                return "Podales nieprawidlowy zapis. Zle zmniejszasz C"
+            elif value[i] == "V" or value[i] == "L" or value[i] == "D" or value[i] == "M":
+                return "Podales nieprawidlowy zapis. Tymi liczbami nie mozna zmniejszać"
+    result = 0
     previous_number = 0
     for i in value[::-1]:
         if dec[i] >= previous_number:
@@ -40,5 +36,4 @@ def rom2dec(value):
     return result
 
 
-print(rom2dec("X"))
-
+print(rom2dec("IM"))
